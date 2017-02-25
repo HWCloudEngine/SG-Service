7f7678ebf7a29b499a0796cffa1dd279f7aefefc
smile-luobin <luobin_smile@163.com>
2017-02-25 11:29:38 +0800

    Set host attribute
    Add set the host attribute of volumes, backups, snapshots;
    Fix bugs in rpcapi, and attach_volume function;
    
    Change-Id: Ic7c99d3a14ecb69732c713d1d70bc9770af77065

--- sgservice/controller/rpcapi.py
+++ sgservice/controller/rpcapi.py
@@ -44,83 +44,83 @@ class ControllerAPI(object):
                                      serializer=serializer)
 
     def enable_sg(self, ctxt, volume):
-        cctxt = self.client.prepare(version='1.0')
+        cctxt = self.client.prepare(server=volume['host'], version='1.0')
         return cctxt.cast(ctxt, 'enable_sg', volume_id=volume.id)
 
     def disable_sg(self, ctxt, volume, cascade=False):
-        cctxt = self.client.prepare(version='1.0')
+        cctxt = self.client.prepare(server=volume['host'], version='1.0')
         return cctxt.cast(ctxt, 'disable_sg', volume_id=volume.id,
                           cascade=cascade)
 
     def attach_volume(self, ctxt, volume, instance_uuid, host_name,
                       mountpoint, mode):
-        cctxt = self.client.prepare(version='1.0')
-        return cctxt.cast(ctxt, 'attach_volume', volume_id=volume.id,
+        cctxt = self.client.prepare(server=volume['host'], version='1.0')
+        return cctxt.call(ctxt, 'attach_volume', volume_id=volume.id,
                           instance_uuid=instance_uuid, host_name=host_name,
                           mountpoint=mountpoint, mode=mode)
 
     def detach_volume(self, ctxt, volume, attachment_id):
-        cctxt = self.client.prepare(version='1.0')
-        return cctxt.cast(ctxt, 'detach_volume', volume_id=volume.id,
+        cctxt = self.client.prepare(server=volume['host'], version='1.0')
+        return cctxt.call(ctxt, 'detach_volume', volume_id=volume.id,
                           attachment_id=attachment_id)
 
     def initialize_connection(self, ctxt, volume, connector):
-        cctxt = self.client.prepare(version='1.0')
+        cctxt = self.client.prepare(server=volume['host'], version='1.0')
         return cctxt.call(ctxt, 'initialize_connection', volume_id=volume.id,
                           connector=connector)
 
     def create_backup(self, ctxt, backup):
-        cctxt = self.client.prepare(version='1.0')
+        cctxt = self.client.prepare(server=backup['host'], version='1.0')
         return cctxt.cast(ctxt, 'create_backup', backup_id=backup.id)
 
     def delete_backup(self, ctxt, backup):
-        cctxt = self.client.prepare(version='1.0')
+        cctxt = self.client.prepare(server=backup['host'], version='1.0')
         return cctxt.cast(ctxt, 'delete_backup', backup_id=backup.id)
 
     def restore_backup(self, ctxt, backup, volume):
-        cctxt = self.client.prepare(version='1.0')
+        cctxt = self.client.prepare(server=backup['host'], version='1.0')
         return cctxt.cast(ctxt, 'restore_backup', backup_id=backup.id,
                           volume_id=volume.id)
 
     def create_snapshot(self, ctxt, snapshot, volume):
-        cctxt = self.client.prepare(version='1.0')
+        cctxt = self.client.prepare(server=snapshot['host'], version='1.0')
         return cctxt.cast(ctxt, 'create_snapshot', snapshot_id=snapshot.id,
                           volume_id=volume.id)
 
     def delete_snapshot(self, ctxt, snapshot):
-        cctxt = self.client.prepare(version='1.0')
+        cctxt = self.client.prepare(server=snapshot['host'], version='1.0')
         return cctxt.cast(ctxt, 'delete_snapshot', snapshot_id=snapshot.id)
 
     def rollback_snapshot(self, ctxt, snapshot, volume):
-        cctxt = self.client.prepare(version='1.0')
+        cctxt = self.client.prepare(server=snapshot['host'], version='1.0')
         return cctxt.cast(ctxt, 'rollback_snapshot', snapshot_id=snapshot.id,
                           volume_id=volume.id)
 
     def create_replicate(self, ctxt, volume):
-        cctxt = self.client.prepare(version='1.0')
+        cctxt = self.client.prepare(server=volume['host'], version='1.0')
         return cctxt.cast(ctxt, 'create_replicate', volume_id=volume.id)
 
     def enable_replicate(self, ctxt, volume):
-        cctxt = self.client.prepare(version='1.0')
+        cctxt = self.client.prepare(server=volume['host'], version='1.0')
         return cctxt.cast(ctxt, 'enable_replicate', volume_id=volume.id)
 
     def disable_replicate(self, ctxt, volume):
-        cctxt = self.client.prepare(version='1.0')
+        cctxt = self.client.prepare(server=volume['host'], version='1.0')
         return cctxt.cast(ctxt, 'disable_replicate', volume_id=volume.id)
 
     def delete_replicate(self, ctxt, volume):
-        cctxt = self.client.prepare(version='1.0')
+        cctxt = self.client.prepare(server=volume['host'], version='1.0')
         return cctxt.cast(ctxt, 'delete_replicate', volume_id=volume.id)
 
     def failover_replicate(self, ctxt, volume):
-        cctxt = self.client.prepare(version='1.0')
+        cctxt = self.client.prepare(server=volume['host'], version='1.0')
         return cctxt.cast(ctxt, 'failover_replicate', volume_id=volume.id)
 
     def reverse_replicate(self, ctxt, volume):
-        cctxt = self.client.prepare(version='1.0')
+        cctxt = self.client.prepare(server=volume['host'], version='1.0')
         return cctxt.cast(ctxt, 'reverse_replicate', volume_id=volume.id)
 
     def create_volume(self, ctxt, snapshot, c_volume):
-        cctxt = self.client.prepare(version='1.0')
+        cctxt = self.client.prepare(server=snapshot['host'], version='1.0')
         return cctxt.cast(ctxt, 'create_volume', snapshot_id=snapshot.id,
                           volume_id=c_volume.id)
