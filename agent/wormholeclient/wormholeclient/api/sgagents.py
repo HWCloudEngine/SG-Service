5522ce9d386be3aef4527dfe745a6cf22f694af7
smile-luobin <luobin_smile@163.com>
2017-08-16 09:43:11 +0800

    Fix
    Change-Id: I3538db538b69d72504f6d0d17ff7aa0750c59dcb

--- agent/wormholeclient/wormholeclient/api/sgagents.py
+++ agent/wormholeclient/wormholeclient/api/sgagents.py
@@ -6,7 +6,7 @@ class SGAgentApiMixin(object):
         return self._result(res, True)
 
     def get_sg_client_status(self, sgclient_properties):
-        data = {'sgagent_properties': sgclient_properties}
+        data = {'sgclient_properties': sgclient_properties}
         url = self._url("/sgagents/get_sg_client_status")
         res = self._post_json(url, data=data)
         self._raise_for_status(res)
